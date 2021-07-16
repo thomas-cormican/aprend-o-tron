d8a0b1263d2c6737f4e80b52fda20830501a51b9
Tomoige <thomascormican123@gmail.com>
2021-07-16 11:48:12 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,124 @@
+import React, {useState} from "react"
+import wordList from "./../word-list.js";
+import Prompt from "./Prompt.jsx";
+import correct from "./../correct.wav"
+import Incorrect from "./../Incorrect.wav"
+
+let dictionary = wordList;
+
+function App() {
+
+  const [randomWord, setRandomWord] = useState(randomWordGenerator());
+  const [userResponse, setUserResponse] = useState("");
+  const [result, setResult] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [score, setScore] = useState(0);
+  const [gameState, setGameState] = useState(true);
+
+  var audioCorrect = new Audio(correct);
+  var audioIncorrect = new Audio(Incorrect);
+
+  function randomWordGenerator() {
+    var index = Math.floor(Math.random() * dictionary.length);
+    return (dictionary[index]);
+  }
+
+  function handleChange(event) {
+    const {value} = event.target;
+    setUserResponse(value);
+  }
+
+  function handleSubmit(event) {
+
+    setIsSubmitted(true);
+    event.preventDefault();
+    var parsedResponse = userResponse.replace(/[^a-zA-Z ]/g, "").toLowerCase();
+    console.log(parsedResponse);
+    dictionary = dictionary.filter(obj => {return obj.word != randomWord.word})
+
+    if (randomWord.translations.includes(parsedResponse)) {
+      console.log("Correct");
+      setResult("Correct");
+      setScore(score =>  score += 1)
+      audioCorrect.play();
+      console.log(score)
+
+    } else {
+      console.log("Incorrect");
+      setResult("Incorrect");
+      audioIncorrect.play();
+    }
+
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter" && event.target.name == "response" && !isSubmitted) {
+      handleSubmit(event);
+    } else if (event.key === "Enter" && isSubmitted)
+      nextWord(event);
+    }
+
+  function nextWord(event) {
+    event.preventDefault();
+    if(dictionary.length > 0){
+    setIsSubmitted(false);
+    setResult("");
+    setUserResponse("");
+    setRandomWord(randomWordGenerator());
+    console.log(gameState)
+  }
+    else {
+      setGameState(false);
+      console.log(gameState);
+
+    }
+  }
+
+  function reset(event) {
+    dictionary = wordList;
+    setGameState(true);
+    setIsSubmitted(false);
+    setResult("");
+    setScore(0);
+    setUserResponse("");
+    setRandomWord(randomWordGenerator());
+  }
+
+  return (<div className="container">
+    <div className="App">
+
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style={{width:100 - dictionary.length + "%"}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+
+
+      <h1>Translate this word</h1>
+
+      <Prompt randomWord={randomWord}/>
+      <form>
+        <textarea className={"form-control"} style={{
+            backgroundColor: "#f7f7f7"
+          }} name="response" rows="5" onChange={handleChange} onKeyDown={handleKeyDown} value={userResponse}/><div className="button-box"><div className="score"><p>Score: {score} / {100 - dictionary.length}</p></div> {
+          isSubmitted
+            ? <button name="continue" className={"btn btn-primary"} onClick={nextWord}>Continue</button>
+            : <button className={"btn btn-primary"} onClick={handleSubmit}>Submit</button>
+        } </div>
+      </form>
+      {
+        isSubmitted && <div>
+            <div className={result === "Correct"? "alert alert-success": "alert alert-danger"} role="alert">
+              <p className={result}>{result}</p>
+              <p>Solution: {randomWord.translations.join(", ")}</p>
+            </div>
+          </div>
+      }
+      {!gameState && <div style={{textAlign: "center"}} className="final-score"><p>Final Score: {score} / {100 - dictionary.length}</p>
+    <button className="btn btn-info" onClick={reset}>Try Again?</button>
+</div>
+      }
+
+    </div>
+  </div>);
+}
+
+export default App;
---
+++ src/components/Prompt.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Prompt(props){
+
+  return (<div className="prompt"><p>{props.randomWord.word}</p></div>)
+
+}
+
+
+export default Prompt;
