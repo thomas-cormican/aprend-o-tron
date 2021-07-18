77613bb8761a81358d0df4efc84d9ed0fd4e4265
Tomoige <thomascormican123@gmail.com>
2021-07-18 09:17:15 +0100

    Fixed bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,38 +33,44 @@ function App() {
     setIsSubmitted(true);
     event.preventDefault();
     var parsedResponse = userResponse.replace(/[^a-zA-Z ]/g, "").toLowerCase();
-    dictionary = dictionary.filter(obj => {
-      return obj.word !== randomWord.word;
-    })
+    console.log(parsedResponse);
+    dictionary = dictionary.filter(obj => {return obj.word != randomWord.word})
 
     if (randomWord.translations.includes(parsedResponse)) {
+      console.log("Correct");
       setResult("Correct");
-      setScore(score => score += 1)
+      setScore(score =>  score += 1)
       audioCorrect.play();
+      console.log(score)
 
     } else {
+      console.log("Incorrect");
       setResult("Incorrect");
       audioIncorrect.play();
     }
+
   }
 
   function handleKeyDown(event) {
-    if (event.key === "Enter" && event.target.name === "response" && !isSubmitted) {
+    if (event.key === "Enter" && event.target.name == "response" && !isSubmitted) {
       handleSubmit(event);
-    } else if (event.key === "Enter" && isSubmitted) {
+    } else if (event.key === "Enter" && isSubmitted)
       nextWord(event);
     }
-  }
 
   function nextWord(event) {
     event.preventDefault();
-    if (dictionary.length > 0) {
-      setIsSubmitted(false);
-      setResult("");
-      setUserResponse("");
-      setRandomWord(randomWordGenerator());
-    } else {
+    if(dictionary.length > 0){
+    setIsSubmitted(false);
+    setResult("");
+    setUserResponse("");
+    setRandomWord(randomWordGenerator());
+    console.log(gameState)
+  }
+    else {
       setGameState(false);
+      console.log(gameState);
+
     }
   }
 
@@ -82,10 +88,9 @@ function App() {
     <div className="App">
 
       <div class="progress">
-        <div class="progress-bar" role="progressbar" style={{
-            width: 100 - dictionary.length + "%"
-          }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+        <div class="progress-bar" role="progressbar" style={{width:100 - dictionary.length + "%"}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
       </div>
+
 
       <h1>Translate this word</h1>
 
@@ -93,37 +98,23 @@ function App() {
       <form>
         <textarea className={"form-control"} style={{
             backgroundColor: "#f7f7f7"
-          }} name="response" rows="5" onChange={handleChange} onKeyDown={handleKeyDown} value={userResponse}/>
-        <div className="button-box">
-          <div className="score">
-            <p>Score: {score}
-              / {100 - dictionary.length}</p>
-          </div>
-          {
-            isSubmitted
-              ? <button name="continue" className={"btn btn-primary"} onClick={nextWord}>Continue</button>
-              : <button className={"btn btn-primary"} onClick={handleSubmit}>Submit</button>
-          }
-        </div>
+          }} name="response" rows="5" onChange={handleChange} onKeyDown={handleKeyDown} value={userResponse}/><div className="button-box"><div className="score"><p>Score: {score} / {100 - dictionary.length}</p></div> {
+          isSubmitted
+            ? <button name="continue" className={"btn btn-primary"} onClick={nextWord}>Continue</button>
+            : <button className={"btn btn-primary"} onClick={handleSubmit}>Submit</button>
+        } </div>
       </form>
       {
         isSubmitted && <div>
-            <div className={result === "Correct"
-                ? "alert alert-success"
-                : "alert alert-danger"} role="alert">
+            <div className={result === "Correct"? "alert alert-success": "alert alert-danger"} role="alert">
               <p className={result}>{result}</p>
               <p>Solution: {randomWord.translations.join(", ")}</p>
             </div>
           </div>
       }
-      {
-        !gameState && <div style={{
-              textAlign: "center"
-            }} className="final-score">
-            <p>Final Score: {score}
-              / {100 - dictionary.length}</p>
-            <button className="btn btn-info" onClick={reset}>Try Again?</button>
-          </div>
+      {!gameState && <div style={{textAlign: "center"}} className="final-score"><p>Final Score: {score} / {100 - dictionary.length}</p>
+    <button className="btn btn-info" onClick={reset}>Try Again?</button>
+</div>
       }
 
     </div>
